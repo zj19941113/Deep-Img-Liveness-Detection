dfcee6a802648d147ddf7e8150860c52712e1e97
zj19941113 <41109686+zj19941113@users.noreply.github.com>
2018-10-19 11:02:14 +0800

    Update liveness_detection.m

--- liveness_detection.m
+++ liveness_detection.m
@@ -1,9 +1,9 @@
-RAW_PATH = '/home/zhoujie/liveness detection/raw文件/face/';
+RAW_PATH = '/home/zhoujie/liveness detection/raw文件/non-face/';
 PLANE_OR_NOT = 0.1;
 ITER = 10000;
 
 file =dir([RAW_PATH ,'*.raw']);
-potable = zeros(1,length(file));
+
 for num=1:length(file)
 f1 = fopen([RAW_PATH,file(num).name], 'r');
 rawname = file(num).name;
@@ -13,7 +13,10 @@ data0 = fread(f1, 'uint16');
 fclose(f1);
 img1 = reshape(data0, 400, 345);
 dep_img = img1';
+% dep_img(find(dep_img > 600))= 0;
+% figure(1),imshow(dep_img,[400,580]); 
 face = dep_img(par2 :par2 +par4,par1 :par1 + par3); 
+% figure(2),imshow(face,[400,580]); 
 %%%三维平面拟合
 [X Y]=meshgrid(1:size(face,2),1:size(face,1));
 zz=face(:);xx=X(:);yy=Y(:);
@@ -23,7 +26,7 @@ data(:,id) = [];
 iter = ITER; 
 %%% 绘制数据点
  number = size(data,2); % 总点数
- bestParameter1=0; bestParameter2=0; bestParameter3=0; % 最佳匹配的参数
+%  bestParameter1=0; bestParameter2=0; bestParameter3=0; % 最佳匹配的参数
  sigma = 1;
  pretotal=0;     %符合拟合模型的数据的个数
 for i=1:iter
@@ -38,22 +41,22 @@ for i=1:iter
      a = ((z(1)-z(2))*(y(1)-y(3)) - (z(1)-z(3))*(y(1)-y(2)))/((x(1)-x(2))*(y(1)-y(3)) - (x(1)-x(3))*(y(1)-y(2)));
      b = ((z(1) - z(3)) - a * (x(1) - x(3)))/(y(1)-y(3));
      c = z(1) - a * x(1) - b * y(1);
-     plane = [a b -1 c];
+%      plane = [a b -1 c];
      mask=abs(plane*[data; ones(1,size(data,2))]);    %求每个数据到拟合平面的距离
      total=sum(mask<sigma);              %计算数据距离平面小于一定阈值的数据的个数
      if total>pretotal            %找到符合拟合平面数据最多的拟合平面
          pretotal=total;
-         bestplane=plane;          %找到最好的拟合平面
+%          bestplane=plane;          %找到最好的拟合平面
     end  
 end
+
 pretotal_ary = pretotal/number;
+% potable(num) = pretotal_ary;
 if(pretotal_ary>PLANE_OR_NOT)
-    descrip = '是平面';
+    descrip = '不是人脸';
 else
-descrip = '不是平面'; 
+descrip = '是人脸'; 
 end
-disp(['pretotal_ary=',num2str(pretotal_ary),',',rawname,descrip]);
+disp(['pretotal=',num2str(pretotal),',','_ary=',num2str(pretotal_ary),',',rawname,descrip]);
 end
-
-
 
