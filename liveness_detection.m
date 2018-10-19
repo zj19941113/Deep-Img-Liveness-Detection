ea7ad96f62f0bdd1ac9f144bf26c3ad37f58e592
zj19941113 <41109686+zj19941113@users.noreply.github.com>
2018-10-19 18:13:17 +0800

    Update liveness_detection.m

--- liveness_detection.m
+++ liveness_detection.m
@@ -41,7 +41,7 @@ for i=1:iter
      a = ((z(1)-z(2))*(y(1)-y(3)) - (z(1)-z(3))*(y(1)-y(2)))/((x(1)-x(2))*(y(1)-y(3)) - (x(1)-x(3))*(y(1)-y(2)));
      b = ((z(1) - z(3)) - a * (x(1) - x(3)))/(y(1)-y(3));
      c = z(1) - a * x(1) - b * y(1);
-%      plane = [a b -1 c];
+     plane = [a b -1 c];
      mask=abs(plane*[data; ones(1,size(data,2))]);    %求每个数据到拟合平面的距离
      total=sum(mask<sigma);              %计算数据距离平面小于一定阈值的数据的个数
      if total>pretotal            %找到符合拟合平面数据最多的拟合平面
